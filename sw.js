f122e6319831d3eca67cd58baa9e114ab55e1577
cdvargasp <vargas.david.7793@gmail.com>
2025-10-22 12:02:31 -0500

    Update cache version from v6 to v7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'uddd-v6';
+const CACHE = 'uddd-v7';
 const ASSETS = [
   './','./index.html','./styles.css','./app.js',
   './manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'
