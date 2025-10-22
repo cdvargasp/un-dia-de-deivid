831a31db0d05f34bce76a74dface6381cc4b931a
cdvargasp <vargas.david.7793@gmail.com>
2025-10-22 11:38:45 -0500

    Update service worker cache version to v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'uddd-v1';
+const CACHE = 'uddd-v2';
 const ASSETS = [
   './','./index.html','./styles.css','./app.js',
   './manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'
