4b5e417a6643270e04693b3ce05b671a8d1aea68
cdvargasp <vargas.david.7793@gmail.com>
2025-10-22 11:53:24 -0500

    Update service worker cache version to v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'uddd-v2';
+const CACHE = 'uddd-v4';
 const ASSETS = [
   './','./index.html','./styles.css','./app.js',
   './manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'
