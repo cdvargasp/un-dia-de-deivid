b6153ef4543c9c8163018fc6fe67c3198ca8cd61
cdvargasp <vargas.david.7793@gmail.com>
2025-10-22 20:37:15 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'uddd-theme-mobile-v1';
+const CACHE = 'uddd-luxury-theme-v1';
 const ASSETS = [
   './','./index.html','./styles.css','./app.js',
   './manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'
@@ -7,7 +7,9 @@ self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
 self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
 });
 self.addEventListener('fetch', e=>{
   e.respondWith(
