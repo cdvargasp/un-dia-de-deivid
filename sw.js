c8794335be65458c5af6c13609607b5e96a82f85
cdvargasp <vargas.david.7793@gmail.com>
2025-10-23 21:57:36 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'uddd-luxury-theme-v5'; // sube versión para forzar recarga
+const CACHE = 'uddd-luxury-theme-v6'; // sube versión para forzar recarga
 const ASSETS = [
   './','./index.html','./styles.css','./app.js',
   './manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'
